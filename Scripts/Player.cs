e1c6268c78ac8b2e51a7ba0fd5e150f3f47e480a
ItaloCobains <italobrandao.contato@gmail.com>
2024-06-25 14:46:08 -0300

    camera configs

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -8,11 +8,36 @@ public partial class Player : CharacterBody3D
 	public  float Speed { get; set; } = 5.0f;
 	[Export]
 	public float JumpVelocity { get; set; } = 4.5f;
+	[Export]
+	public float MouseSensitivity { get; set; } = 0.1f;
+	[Export]
+	public int MaxHealth { get; set; } = 100;
+	[Export]
+	public int CurrentHealth { get; set; } = 100;
+	[Export]
+	public string Name { get; set; } = "Player";
+
+	private Camera3D _camera;
+	private Vector2 _mouseDelta = Vector2.Zero;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
+	public override void _Ready()
+	{
+		_camera = GetNode<Camera3D>("Camera");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+
+    public override void _Input(InputEvent @event)
+    {
+			if (@event is InputEventMouseMotion mouseMotionEvent)
+			{
+				_mouseDelta = mouseMotionEvent.Relative;
+			}
+    }
+
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
 
@@ -38,6 +63,13 @@ public partial class Player : CharacterBody3D
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
 		}
+
+		RotateY(Mathf.DegToRad(-_mouseDelta.x * MouseSensitivity));
+		_camera.RotateX(Mathf.DegToRad(-_mouseDelta.y * MouseSensitivity));
+
+		Vector3 cameraRotation = _camera.RotationDegrees;
+		cameraRotation.x = Mathf.Clamp(cameraRotation.x, -90, 90);
+		_camera.RotationDegrees = cameraRotation;
 
 		Velocity = velocity;
 		MoveAndSlide();
